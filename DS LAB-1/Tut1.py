f2094ae5a82a5b9a6835a39741d3370b763d3fb1
amnaasim24 <142867835+amnaasim24@users.noreply.github.com>
2024-08-21 10:23:39 +0500

    Add files via upload

---
+++ DS LAB-1/Tut1.py
@@ -0,0 +1,43 @@
+a = int(input("Enter an Integer: "))
+b = float(input("Enter a Floating number: "))
+c = input("Enter your Name: ")
+print(a)
+print(b)
+print(c)
+
+d = {"Purple", "Blue", "Green"}
+e = ["Purple", "Blue", "Green"]
+print(d)
+print(type(d))
+print(e)
+print(type(e))
+
+f = {"Amna":"23K0859", "Riya":"23K0063", "Rija":"23K0057"}
+g = {"Name":["Amna", "Riya", "Rija"], "ID":["23K-0859", "23K-0063", "23K-0057"]}
+print(f)
+print(type(f))
+print(g)
+print(type(g))
+
+h = 4
+i = 2
+print(h+i)
+print(h-i)
+print(h*i)
+print(h/i)
+
+j = 90
+k = 50
+if j>k:
+    print("Test Cleared!")
+elif j>85:
+    print("Test Distinction")
+else:
+    print("Test Failed!")
+
+l = 0
+while l<5:
+    print("PAI")
+    l = l+1
+
+for m in range
