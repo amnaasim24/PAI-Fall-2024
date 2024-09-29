77a229831f5ffabcf939323369f347b2197a2295
amnaasim24 <142867835+amnaasim24@users.noreply.github.com>
2024-09-29 23:37:36 +0500

    Add files via upload

---
+++ PAI LAB-6/Lab6-Task7/Lab6-Task7.py
@@ -0,0 +1,10 @@
+# Programming for AI LAB - FALL 2024
+# Lab - 6
+
+import pandas as pd
+
+df = pd.read_excel('employee.xlsx', engine='openpyxl')
+
+specified_year = 2020
+employees_in_year = df[df['Year'] == specified_year]
+print(employees_in_year)
