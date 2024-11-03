e51c1b703138052bd0ac7a3508936176491b5ef1
amnaasim24 <142867835+amnaasim24@users.noreply.github.com>
2024-11-03 17:29:53 +0500

    Add files via upload

---
+++ PAI LAB-9/Lab9-Task3.py
@@ -0,0 +1,13 @@
+# Programming for AI LAB - FALL 2024
+# Lab - 9
+
+import matplotlib.pyplot as plt
+
+flavors = ['Vanilla', 'Chocolate', 'Strawberry', 'Mint', 'Cookie Dough']
+scoops_sold = [150, 100, 80, 60, 90]
+
+plt.figure(figsize=(8, 6))
+plt.pie(scoops_sold, labels=flavors, autopct='%1.1f%%', startangle=140)
+plt.title('Distribution of Ice Cream Sales')
+plt.axis('equal')
+plt.show()
